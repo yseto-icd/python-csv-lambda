2f24898fde291c7f77a3e69f87913164577ba3e4
yseto-icd <yuji.seto@infcurion.com>
2022-06-02 12:09:56 +0900

    s3接続部分メソッド化

--- src/csv.py
+++ src/csv.py
@@ -1,26 +1,15 @@
-import urllib.parse
 import csv
-import boto3
+import s3_client
 import io
 
 print('Loading function')
 
-s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
+    response = s3_client.fetch_binary(event)
 
-    # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
     try:
         data = response['Body'].read()
         contents = data.decode('utf-8')
---
+++ src/s3_client.py
@@ -0,0 +1,16 @@
+import urllib.parse
+import boto3
+
+s3 = boto3.client('s3')
+
+def fetch_binary(event):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+        return response
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
